3ac4663800ca4bcff6e8d6c089ed1d2276b4dc2e
Daniel Ortiz <do973658@wtstudents.wtamu.edu>
2019-03-28 12:31:14 -0500

    Commit agian

--- Pages/Page.cshtml.cs
+++ Pages/Page.cshtml.cs
@@ -5,12 +5,34 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace Lab10.Pages
 {
     public class NewModel : PageModel
     {
         private readonly ILogger _log;
+
+        [BindProperty]
+        [Display(Name = "First Name")]
+        [Required]
+        [StringLength(30)]
+        public string FirstName {get; set;}
+
+        [BindProperty]
+        [Display(Name = "Last Name")]
+        [Required]
+        [StringLength(30)]
+        public string LastName {get; set;}
+
+        [BindProperty]
+        [Display(Name = "Credit Card Number")]
+        [Required]
+        [CreditCard]
+        [StringLength(20)]
+        public string CCNumber {get; set;}
         public NewModel (ILogger<IndexModel> log)
         {
             _log = log;
@@ -21,7 +43,8 @@ namespace Lab10.Pages
         }
         public void OnPost()
         {
-            _log.LogWarning("Welcome!");
+            _log.LogWarning(FirstName);
+            _log.LogWarning(LastName);
         }
     }
 }
